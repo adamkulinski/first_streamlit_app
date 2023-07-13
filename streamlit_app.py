d3c21f66d5c211094b2b231c53da449af9ac07be
Adam Kuliński <47415989+adamkulinski@users.noreply.github.com>
2023-07-13 14:52:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text('Omega 3 & Blueberry Oatmeal 🥣 ')
 streamlit.text('Kale, Spinach & Rocket Smoothie 🥗 🥑🍞')
 streamlit.text('Hard-Boiled Free-Range Egg 🐔')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
