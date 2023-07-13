096d763bc948db87e9ac6d19e8eb1076d3ddc66c
Adam Kuliński <47415989+adamkulinski@users.noreply.github.com>
2023-07-13 14:52:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
 streamlit.header('Breakfast Menu')
-
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-streamlit.title('Kale, Spinach & Rocker')
-streamlit.title('Mdadsasddsar')
-
+streamlit.text('Omega 3 & Blueberry Oatmeal 🥣 ')
+streamlit.text('Kale, Spinach & Rocket Smoothie 🥗 🥑🍞')
+streamlit.text('Hard-Boiled Free-Range Egg 🐔')
 
