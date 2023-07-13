2e9e9870c949754290e79af336d3d59e28f74e24
Adam Kuliński <47415989+adamkulinski@users.noreply.github.com>
2023-07-13 14:53:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit
+import pandas as pd
+
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 
 streamlit.title('My parents new healthy diner')
 
