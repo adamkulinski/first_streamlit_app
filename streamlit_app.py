92fd06443b8e28f1d5bcb98dc86636a70ab3819f
Adam Kuliński <47415989+adamkulinski@users.noreply.github.com>
2023-07-13 14:55:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie 🥗 🥑🍞')
 streamlit.text('Hard-Boiled Free-Range Egg 🐔')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
